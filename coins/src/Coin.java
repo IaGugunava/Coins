5faa6056b8b33b430ee35faf8d16a720fd7622b9
ia <iagugunava05@gmail.com>
2022-07-02 20:31:27 +0400

    Initial commit

---
+++ coins/src/Coin.java
@@ -0,0 +1,34 @@
+public class Coin
+{
+    // m is size of coins array (number of different coins)
+    static int minSplit(int coins[], int m, int V)
+    {
+        // base case
+        if (V == 0) return 0;
+
+        // Initialize result
+        int res = Integer.MAX_VALUE;
+
+        // Try every coin that has smaller value than V
+        for (int i=0; i<m; i++)
+        {
+            if (coins[i] <= V)
+            {
+                int sub_res = minSplit(coins, m, V-coins[i]);
+
+                // Check for INT_MAX to avoid overflow and see if
+                // result can minimized
+                if (sub_res != Integer.MAX_VALUE && sub_res + 1 < res)
+                    res = sub_res + 1;
+            }
+        }
+        return res;
+    }
+    public static void main(String args[])
+    {
+        int coins[] =  {1, 5, 10, 20, 50};
+        int m = coins.length;
+        int V = 80;
+        System.out.println("Minimum coins required is "+ minSplit(coins, m, V) );
+    }
+}
